eb199b30f1e7981edd5a62055f72433346a81036
Anxinal <ax1375240926@gmail.com>
2025-05-20 13:13:26 +0800

    Construct the basic structure for the login page

---
+++ CyberCatMain/app/login.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput
+} from 'react-native';
+import { Link } from 'expo-router'; // or from 'expo-router' if you’re using Expo Router
+import PropTypes from 'prop-types';
+
+const Login = () => {
+  const [userName, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const Info = ({ label, setFunction, value}) => (
+    <View style={styles.inputContainer}>
+      <Text style={styles.infoText}>{label}:</Text>
+      <TextInput
+        style={styles.inputStyle}
+        onChangeText={setFunction}
+        placeholder={label}
+        placeholderTextColor="grey"
+        value = {value}
+      />
+    </View>
+  );
+  Info.propTypes = {
+    label: PropTypes.string.isRequired,
+    setFunction: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired
+  };
+
+  const loginPress = () => {
+    console.log(`Username: ${userName}\nPassword: ${password}`);
+  };
+  const registerPress = () => {
+    console.log('Register query accepted.');
+  };
+
+  return (
+    <View>
+      <View style={styles.logoContainer}>
+        <Text style={styles.logoText}>Logo goes here</Text>
+      </View>
+
+      <View style={styles.loginInputContainer}>
+        <Info label="Username" setFunction={setUsername} value = {userName} />
+        <Info label="Password" setFunction={setPassword} value = {password} />
+      </View>
+
+      <View style={styles.buttonsContainer}>
+
+          <Button style ={styles.button} title ='login' onPress = {loginPress}/>
+          <Button style ={styles.button} title='Register' onPress = {registerPress}/>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+
+  logoContainer: {
+    flex: 8,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logoText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  loginInputContainer: {
+    flex: 3,
+    justifyContent: 'center'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8
+  },
+  infoText: {
+    flex: 3,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  inputStyle: {
+    flex: 7,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    height: 40,
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  buttonsContainer: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 50,
+    marginBottom:10,
+    marginHorizontal: 'auto',
+    justifyConten: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 4
+  },
+  button: {
+    width: '100%',
+    height: 60,
+    marginHorizontal: 'auto',
+    backgroundColor: 'blue',
+    marginTop: 20,
+    marginBottom: 10,
+    marginleft: 10,
+    marginRight: 20,
+  }
+});
