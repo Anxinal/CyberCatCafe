764c1a26e76c0d2d041235d1c1a7c76de2ccd9d1
Anxinal <ax1375240926@gmail.com>
2025-05-26 18:39:05 +0800

    Debug the reset function,implement consistent two-digit time display  and beautify the UI

--- CyberCatMain/app/mainPages/Timer/screen.jsx
+++ CyberCatMain/app/mainPages/Timer/screen.jsx
@@ -1,18 +1,22 @@
 import { useState, useRef} from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native'
+import { TimeView } from "./TimeView.tsx"
 export default function Timer() {
   const [startTime, setStartTime] = useState(null);
   const [now, setNow] = useState(null);
-  const [totalTime, setTotalTime] = useState(10000);
+  const [totalTime, setTotalTime] = useState(100);
+  const [remained, setRemained] = useState(totalTime);
   const intervalRef = useRef(null);
-  let remained = 0;
-  let min = 0;
-  let sec = 0;
+  let currentTime = remained;
   let paused = useRef(true);
+  
   function handleStart() {
     // Start counting.
     paused.current = false;
     console.log(totalTime)
+    if(remained <= 0){
+      setRemained(totalTime);
+    }
     setStartTime(Date.now());
     setNow(Date.now());
     clearInterval(intervalRef.current);
@@ -24,50 +28,52 @@ export default function Timer() {
   
   function handleStop(){
     paused.current = true;
-     clearInterval(intervalRef.current);
-     setTotalTime(totalTime - (now - startTime));
+    clearInterval(intervalRef.current);
+    setRemained(currentTime);
   }
 
   function handleReset(){
-    clearInterval(intervalRef.current);
-    paused = true;
-    remained = -1;
+    handleStop();
+    setRemained(totalTime);
   }
-  if (startTime != null && now != null) {
-    if(remained >= 0){
-      remained = (totalTime - (now - startTime)) / 1000;
-      if (paused.current) {
-        remained = totalTime / 1000;
-      }
-      console.log(paused)
-    }
 
+  if (startTime != null && now != null) {
+
+    if(remained >= 0 && !paused.current){
+      currentTime = remained - ((now - startTime) / 1000);
+      console.log(paused.current)
+    }
     if(!paused.current && remained < 0){
         handleStop();
     }
-
-    min = remained > 0 ? Math.round(remained / 60) : 0;
-    sec = remained > 0 ? Math.round(remained % 60) : 0;
   }
+
   return (
-    <View>
-      <Text style ={{color:'white', fontSize: 30}}>Time passed: {min} minutes {sec} seconds</Text>
-      <button onClick={handleStart} style = {{width: 200, height: 100, marginLeft: 20}}>
-        Start
-      </button>
-      <button onClick = {handleStop} style = {{width: 200, height: 100, marginLeft: 20}}> 
-        Stop
-      </button>
-      <button onClick = {handleReset} style = {{width: 200, height: 100, marginLeft: 20}}> 
-        Reset
-      </button>
+    <View style = {{flexDirection:'column', alignContent:'center', padding: 1}}>
+      <Text style ={{color:'white', fontSize: 30}}>Time passed: </Text>
+       <View><TimeView min = {currentTime > 0 ? Math.floor(currentTime / 60) : 0} 
+                       sec = {currentTime > 0 ? Math.floor(currentTime % 60) : 0} 
+                       stopped = {paused.current}/></View>
+    <View style ={styles.ButtonView}>    
+      <TouchableOpacity onPress={handleStart} style = {styles.Buttons}>
+        <Text style ={{fontSize: 19, fontWeight: 'bold', color: 'white'}}>Start</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress = {handleStop} style = {styles.Buttons}> 
+        <Text style ={{fontSize: 19, fontWeight: 'bold', color: 'white'}}>Stop</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress = {handleReset} style = {styles.Buttons}> 
+        <Text style ={{fontSize: 19, fontWeight: 'bold', color: 'white'}}>Reset</Text>
+      </TouchableOpacity>
+      </View>
+     
       <TextInput style = {styles.TimeSet}
-                 onChangeText={x => setTotalTime(parseInt(x) * 1000)}
-                 editable = {paused}
+                 onChangeText={x => setTotalTime(parseInt(x))}
+                 editable = {paused.current}
                   />
     </View>  
 );
   }
+
 const styles = StyleSheet.create({
   TimeSet: {
     color: 'white',
@@ -79,5 +85,31 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: 'white',
     borderWidth: 1,
+  },
+  Buttons: {
+    width: 60,
+    height: 40, 
+    marginLeft: 20,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    borderRadius: 10,
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 4,
+
+  },
+  ButtonView: {
+    width: '100%',
+    maxWidth: 500,
+    flex: 5,
+    maxHeight: 500,
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    borderColor: 'gray',
+    borderRadius: 10,
+    borderWidth: 2,
+    padding: 4, 
+
   }
+ 
 });
