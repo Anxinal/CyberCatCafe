f134d10f027bc3ceebb629ca9f548661fb5fed42
Anxinal <ax1375240926@gmail.com>
2025-05-29 21:00:35 +0800

    Initialise the user center page for backend debug testing

---
+++ CyberCatMain/app/mainPages/userCenter.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {Text, View} from "react-native"
+import { app } from "../../firebasecConfig.js"
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { Maybe } from "@/data/Maybe.js"; 
+export default function userCenter({userId} : {userId: string}) {
+  const db = getFirestore(app);
+  const docRef = doc(db, "users/" + userId, "profile");
+  let userData = null;
+  const docSnap =  getDoc(docRef).then( (doc) => {
+      if (doc.exists()) {
+        console.log("Document data:", doc.data());
+        userData = doc.data();
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+  }
+  ).catch((error) => {
+    console.log("Error getting document:", error);
+});
+
+  return  (
+  <Text> This is to test the user Id function  </Text>
+);
+}
+
