8c5c61fb657b181a47822573e6a0830a3043f66d
Anxinal <ax1375240926@gmail.com>
2025-06-01 19:30:29 +0800

    Improve the logic of data quries from firebase

--- CyberCatMain/app/account/userInfo.js
+++ CyberCatMain/app/account/userInfo.js
@@ -1,11 +1,14 @@
 import { app } from "../../firebasecConfig.js"
-import { getFirestore, doc, getDoc, collection,setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, collection,setDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, 
          signInWithEmailAndPassword, signOut,
          createUserWithEmailAndPassword } from "firebase/auth";
 const db = getFirestore(app);
 const collectionRef = collection(db, "users");
 const auth = getAuth();
+
+// 0000 is the default user id that stores all default user information in the database
+let currentUser = "0000";
 
 
 const navigateToMain = (router) => {router.push('mainPages/userCenter');};
@@ -15,32 +18,48 @@ const navigateToMain = (router) => {router.push('mainPages/userCenter');};
  app page(with useState) so that the page can be re-rendered after the information is retrieved 
  */
 export function getUserInfo(attribute, setFunction){
- 
- onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    
-    getDoc(doc(collectionRef, user.uid)).then( (doc) => {
-      if (doc.exists()) {
-        console.log("OK");
-        setFunction(doc.data()[attribute]);
-      }else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    }).catch((error) => {
+
+  //Apply memoisation to the userdata obtained from firebase
+  let cachedUserData = null;
+
+  function requireUserdataFromFirebase(attribute, setFunction){
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+     // User is signed in, see docs for a list of available properties
+     // https://firebase.google.com/docs/reference/js/auth.user
+      currentUser = user;
+
+      getDoc(doc(collectionRef, user.uid)).then( (doc) => {
+        if (doc.exists()) {
+          console.log("OK");
+          cachedUserData = doc.data();
+          setFunction(cachedUserData[attribute]);   
+        }else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      }).catch((error) => {
         console.log("Error getting document:", error);
         return error;
-  });
-    // ...
+    });
   } else {
     // User is signed out
-    // ...
+    currentUser = null;
+    
   }
-});
+  });
+  }
+
+  onSnapshot(doc(collectionRef, currentUser.uid), (doc) => {
+    cachedUserData = doc.data();
+  });
+
+  if(allUserdata == null) requireUserdataFromFirebase(attribute, setFunction);
+  setFunction(cachedUserData[attribute]);
   
 }
+
+
 
 /* This function takes in the user informations in the log in page and update the 
 login status asynchronously.
@@ -52,7 +71,7 @@ export function signInUser(email, password, router){
   return signInWithEmailAndPassword(auth, email, password)
          .then((userCredential) => {
             // Signed in 
-            console.log("login success" + userCredential.user.email);
+            alert("login success" + userCredential.user.email);
             navigateToMain(router);
             return "";
          })
@@ -92,5 +111,12 @@ export function registerNewUser(email, password, username){
         }).catch((error) => {
           return error.message;
     }); 
+
+}
+
+export function updateUserInfo(attribute, value){
+  updateDoc(doc(collectionRef, currentUser.uid), {
+    [attribute] : value
+  });
 
 }
