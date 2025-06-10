56ff558e89c86a5ca6f81eb19c63df9751b6d96f
Anxinal <ax1375240926@gmail.com>
2025-06-10 13:46:33 +0800

    [Improvement] Improve the timer display mechanism

--- CyberCatMain/app/mainPages/Timer/TimeView.tsx
+++ CyberCatMain/app/mainPages/Timer/TimeView.tsx
@@ -3,13 +3,9 @@ import {Text, View} from 'react-native';
 import React from 'react';
 import { timerStyles } from '@/constants/TimerStyles';
 import catImage from '../../../assets/images/timer-frame.png'
-
+import { getMinView, getSecView } from '@/data/TimerConvert';
 
 const timeColor = ({stopped}:{stopped: boolean;}) => (stopped ? 'red' : 'green');
-const adjustTimeView = ({n}:{n: number;}) : string => (n < 10 ? "0" + n : n + "");
-
-const getmin = (currentTime: number) => (currentTime > 0 ? Math.floor(currentTime / 60) : 0);
-const getsec = (currentTime: number) => (currentTime > 0 ? Math.floor(currentTime % 60) : 0);
 
 //TODO: Update the timer cat animation later
 export const TimeView = ({currentTime, stopped}: 
@@ -19,7 +15,7 @@ export const TimeView = ({currentTime, stopped}:
      (<View style = {timerStyles.countdownView}>
         <ImageBackground source={catImage} contentFit = "contain" style = {{width: 400, height: 350}}>
             <Text style = {[timerStyles.countdownText, {color: timeColor({stopped})}]}>
-                {adjustTimeView({n :getmin(currentTime)})} : {adjustTimeView({n: getsec(currentTime)})}
+                {getMinView(currentTime)} : {getSecView(currentTime)}
             </Text> 
         </ImageBackground>
             
