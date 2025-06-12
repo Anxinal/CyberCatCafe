950c54c6dcfa839ff2c4abbd96b3cd8847793003
Anxinal <ax1375240926@gmail.com>
2025-06-12 20:58:35 +0800

    [Fun}Update the about page

--- CyberCatMain/app/mainPages/UserCenter/Settings/About.jsx
+++ CyberCatMain/app/mainPages/UserCenter/Settings/About.jsx
@@ -7,11 +7,16 @@ const About = () =>  (
     <View>
       <TitleComp content={'About Cyber Cat Cafe'} /> 
       <Text style ={styles.content}>
-        Never expected that you have arrived here. Let me just finish my job here.
+        Never expected that you have arrived here. Let me just finish my job first.
       </Text>
        <Text style ={styles.content}>The Cyber Cat Cafe application is developed by AnXiousgrassHoppeR
         With special thanks to XXX who has showcased his/her artistic talent in the design of the cats.
-        Oh That's all, have a nice day.
+        </Text> 
+         <Text style ={styles.content}>Additionally, Mr. Anonymous, who does not want to disclose his true identity,
+           is keen to showcase his muscial virtuosity as a member of the team. His passion in the feline sound has 
+           transmorgrified the simple combination of string instruments into a perfect composition for our CyberCatCafe.
+        </Text> 
+        <Text style ={styles.content}> That's all so far, have a nice day! XD
         </Text> 
       <HrefLink href = {"/mainPages/UserCenter/screen"} text= {"Back to User Center"}/>
     </View>
@@ -22,5 +27,5 @@ const About = () =>  (
 export default About;
 
 const styles = StyleSheet.create({
-    content: {fontSize: textFont.small + 2, color: textColor.normal},
+    content: {fontSize: textFont.small + 2, color: textColor.normal, marginBottom: 20,},
 });
