d409ab320993bc8a08f8f8511e09ef2365bd3ca3
Anxinal <ax1375240926@gmail.com>
2025-06-16 17:26:23 +0800

    [UI] Update some UI settings

--- CyberCatMain/app/mainPages/Achievement/achievement.js
+++ CyberCatMain/app/mainPages/Achievement/achievement.js
@@ -1,21 +1,11 @@
 
 import { achievementList } from '../../../constants/achievementList.js';
+import { StyleSheet, Dimensions ,View ,Text} from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
-// export const updateAchievementStatus = async () => {
-//  const updatedList = [];
-//  return Promise.resolve(
-//     achievementList.forEach((achievement) => {
-//         achievement.criteria().then((condition)=>{
-//             return condition;
-//         }).then((condition) => { 
-//          updatedList.push({
-//         ...achievement,
-//         completed: condition
-//       });
-//     });
-//     })).then(() => updatedList);
-  
-// }
+const LockedIcon = () => <FontAwesome5 name="question" size={30} color="black" />;
+const UnlockedIcon = () => <FontAwesome5 name="star" size={30} color="rgb(245, 237, 14)" />;
+const screenWidth = Dimensions.get('window').width;
 
 export async function updateAchievementStatus() {
   // Kick off one promise per achievement:
@@ -39,3 +29,52 @@ export const processList = (newlist, filterFunc, sortFunc) => {
     return newlist.filter(item => !(item.hidden && !item.completed) && filterFunc).sort(sortFunc);
     
  }
+ export const countAttainedAchievements = (achList) => achList.filter(item => item.completed).length;
+
+ export const AchievementView = ({item}) => {
+    return  (<View style={[styles.card, item.completed ? styles.cardComplete : styles.cardIncomplete]}>
+                            {item.completed ?
+                                (<UnlockedIcon/>) : (<LockedIcon/>)
+                            }
+                            <View style={styles.box}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <Text style={styles.description}>{item.description}</Text>
+                            </View>
+                        </View>);
+ }
+
+ const styles = StyleSheet.create({
+
+     card: {
+         flexDirection: 'row',
+         padding: 30,
+         width: screenWidth,
+         marginBottom: 10,
+         marginTop: 10,
+         borderRadius: 10,
+         marginHorizontal: 'auto',
+     },
+     cardComplete: {
+         backgroundColor: 'pink',
+         borderWidth: 3,
+         borderColor: 'rgb(245, 237, 14)'
+     },
+     cardIncomplete: {
+         backgroundColor: 'grey',
+     },
+     box: {
+         flex: 1,
+         marginLeft: 20,
+         flexDirection: 'column',
+     },
+     title: {
+         color: 'white',
+         fontSize: 20,
+         fontWeight: 'bold'
+     },
+     description: {
+         color: 'black',
+         fontSize: 15,
+     },
+ }
+ );
--- CyberCatMain/app/mainPages/Achievement/screen.jsx
+++ CyberCatMain/app/mainPages/Achievement/screen.jsx
@@ -1,14 +1,13 @@
 import { View, Text, Button, StyleSheet, FlatList, Dimensions } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { achievementList } from '../../../constants/achievementList'
-import { updateAchievementStatus, processList } from './achievement'
+import { updateAchievementStatus, processList, AchievementView, countAttainedAchievements } from './achievement'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const screenWidth = Dimensions.get('window').width
 
 export default function screen() {
 
-    
     let [orderedAch, setOrderedAch] = useState([]);
     console.log("rendered");
     useEffect( () => {
@@ -21,26 +20,19 @@ export default function screen() {
          });
          },[]);
   
-
+    
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.text}>Achievements</Text>
+            <Text> Total attained achievements: {countAttainedAchievements(orderedAch)}</Text>
             <FlatList
                 data={orderedAch}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                    <View style={[styles.card, item.completed ? styles.cardComplete : styles.cardIncomplete]}>
-                        {item.completed ?
-                            (<Text>icon</Text>) : (<Text>question</Text>)
-                        }
-                        <View style={styles.box}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <Text style={styles.description}>{item.description}</Text>
-                        </View>
-                    </View>
+                    <AchievementView item={item} />
                 )}
             />
-            <Text>navigationBar</Text>
+            <Text>End of Achievement List</Text>
         </SafeAreaView>
     );
 }
@@ -88,6 +80,6 @@ const styles = StyleSheet.create({
         fontSize: 15,
     },
 }
-)
+);
 
 
