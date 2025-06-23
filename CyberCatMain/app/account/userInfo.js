1267bd7f37c3af7454810357d27952e9c904eef9
haorui2004 <riahao4511@gmail.com>
2025-06-23 14:04:21 +0800

    Styling changes

--- CyberCatMain/app/account/userInfo.js
+++ CyberCatMain/app/account/userInfo.js
@@ -1,11 +1,13 @@
 import { app } from "../../firebaseConfig.js"
-import { getFirestore, doc, getDoc, collection,setDoc, updateDoc } from "firebase/firestore";
-import { getAuth, onAuthStateChanged, 
-         signInWithEmailAndPassword, signOut,
-         createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, doc, getDoc, collection, setDoc, updateDoc } from "firebase/firestore";
+import {
+  getAuth, onAuthStateChanged,
+  signInWithEmailAndPassword, signOut,
+  createUserWithEmailAndPassword
+} from "firebase/auth";
 import { UserInitInfo } from '../../constants/UserInitialInfo.js'
 import { useRouter } from "expo-router";
-import {displayError, displayNull, displayToast} from '../../components/ToastMessage.js'
+import { displayError, displayNull, displayToast } from '../../components/ToastMessage.js'
 
 const db = getFirestore(app);
 const collectionRef = collection(db, "users");
@@ -15,40 +17,40 @@ const auth = getAuth();
 let currentUser = "0000";
 
 
-const navigateToMain = (router) => {router.push('mainPages/UserCenter/screen');};
-const navigateToLogin = () => {useRouter().push("/")};
+const navigateToMain = (router) => { router.push('mainPages/UserCenter/screen'); };
+const navigateToLogin = () => { useRouter().push("/") };
 
 
 /* This function takes in an attribute of the user(such as username) and retrieve the data from the database
  It returns nothing and since the process is async, another set function is required for a temporary variable in the 
  app page(with useState) so that the page can be re-rendered after the information is retrieved 
 */
-export function getUserInfo(attribute, setFunction){
- 
- onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    
-    getDoc(doc(collectionRef, user.uid)).then( (doc) => {
-      if (doc.exists()) {
-        currentUser = user.uid;
-        setFunction(doc.data()[attribute]);
-      }else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    }).catch((error) => {
+export function getUserInfo(attribute, setFunction) {
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+
+      getDoc(doc(collectionRef, user.uid)).then((doc) => {
+        if (doc.exists()) {
+          currentUser = user.uid;
+          setFunction(doc.data()[attribute]);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      }).catch((error) => {
         console.log("Error getting document:", error);
         return error;
+      });
+
+    } else {
+      // User is signed out
+      unsubscribe();
+    }
   });
-  
-  } else {
-    // User is signed out
-    unsubscribe();
-  }
-});
-  
+
 }
 
 
@@ -58,22 +60,22 @@ login status asynchronously.
 It returns the promise chain to update login status itself which returns a string that represents the login status
 use then() if the error message needs to be used.
  */
-export function signInUser(email, password, router){
-   if(email == "" || password == "")  return displayNull(); 
+export function signInUser(email, password, router) {
+  if (email == "" || password == "") return displayNull();
   return signInWithEmailAndPassword(auth, email, password)
-         .then((userCredential) => {
-            // Signed in 
-            displayToast("Login Success");
-            navigateToMain(router);
-         }).catch(displayError("Meow? Wrong password?"));
-    
+    .then((userCredential) => {
+      // Signed in 
+      displayToast("Login Success");
+      navigateToMain(router);
+    }).catch(displayError("Meow? Wrong password?"));
+
 }
 
 /* This function signs out the current user in the device and returns nothing
  */
-export function signOutUser(){
-  signOut(auth).then(() => {console.log("signed out successfully")})
-               .catch(console.log);
+export function signOutUser() {
+  signOut(auth).then(() => { console.log("signed out successfully") })
+    .catch(console.log);
 }
 
 /* This function takes in the user information in the register page and update the 
@@ -84,15 +86,15 @@ does not update the login status
 It returns the promise chain to update login status itself which returns a string that represents the login status
 use then() if the error message needs to be used.
  */
-export function registerNewUser(email, password, username){  
-      console.log('Register query accepted.');
-      if(email == "" || password == "" || username == "")  return displayNull(); 
-      return createUserWithEmailAndPassword(auth, email, password)
-      .then(async (userCredential) => {
-        // add some initial information to the user account
-        await setDoc(doc(db, "users", userCredential.user.uid),
+export function registerNewUser(email, password, username) {
+  console.log('Register query accepted.');
+  if (email == "" || password == "" || username == "") return displayNull();
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then(async (userCredential) => {
+      // add some initial information to the user account
+      await setDoc(doc(db, "users", userCredential.user.uid),
         UserInitInfo(username));
-        }).then(navigateToLogin).catch(displayError("Register failed")); 
+    }).then(navigateToLogin).catch(displayError("Register failed"));
 }
 
 /*
@@ -102,25 +104,24 @@ Value: The new value of the attribute
 If you want to reload the page remember that you should do it manually in your app page
 For example use .then(setFunction) to trigger re-render
 */
-export function updateUserInfo(attribute, value){
+export function updateUserInfo(attribute, value) {
 
   return updateDoc(doc(collectionRef, currentUser), {
-    [attribute] : value
+    [attribute]: value
   }).then(() => console.log("Update attribute success")).catch(console.log);
 
 }
 
-export function mapUserInfo(attribute, mapFunction){
-  
+export function mapUserInfo(attribute, mapFunction) {
+
   return getDoc(doc(collectionRef, currentUser))
     .then(
-    (doc) => doc.data()[attribute]).then((original) => 
-      {
+      (doc) => doc.data()[attribute]).then((original) => {
         updateDoc(doc(collectionRef, currentUser),
-        {
-           [attribute] : mapFunction(original)
-        })
-        .then(() => "Information Update Successful").catch(err => "Updated Failed because (of)" + err.message);
+          {
+            [attribute]: mapFunction(original)
+          })
+          .then(() => "Information Update Successful").catch(err => "Updated Failed because (of)" + err.message);
       });
 
 }
