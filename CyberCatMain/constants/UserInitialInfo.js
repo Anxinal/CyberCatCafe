c2ce648a078a2e56a47e99061fe22f66fc3fabce
haorui2004 <riahao4511@gmail.com>
2025-06-25 11:39:57 +0800

    added the pet field to userInitialInfo

--- CyberCatMain/constants/UserInitialInfo.js
+++ CyberCatMain/constants/UserInitialInfo.js
@@ -1,23 +1,25 @@
-export const UserInitInfo = (username) => ( {
-  
+export const UserInitInfo = (username) => ({
+
   totalFocus: 0,
 
   username: username,
   listOfAchievements: [1],
 
-  listOfCats: [
+  pet: [
     {
-        id: 0,
-        growthStatus: 0,
+      id: 0,
+      level: 0,
+      current_skin: "default skin",
+      unlocked_skins: ["default skin"],
     }
   ],
-  
+
   focusSessionCount: 0,
   coins: 500,
 
   lastLoginDate: 0,
   ConsecutiveLoginDays: 0,
   TotalLoginDays: 0,
-  
+
 });
 // For totalFocus, it refers to total focus time which is recorded in seconds.
