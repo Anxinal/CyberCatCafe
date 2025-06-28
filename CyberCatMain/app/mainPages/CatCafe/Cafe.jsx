4dd77379cad883160864777ac198e2fa6dd2ac28
haorui2004 <riahao4511@gmail.com>
2025-06-28 14:36:27 +0800

    UI changes to the cat feeding function

--- CyberCatMain/app/mainPages/CatCafe/Cafe.jsx
+++ CyberCatMain/app/mainPages/CatCafe/Cafe.jsx
@@ -1,5 +1,5 @@
-import { View, Text, SafeAreaView, ImageBackground, Button } from 'react-native'
-import React, { useRef } from 'react'
+import { View, Text, SafeAreaView, ImageBackground, Button, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useRef, useState } from 'react'
 import catBackground from '@/assets/images/cafe-background.jpg'
 import { Cat } from './Cat.tsx'
 import { CatFoodCan } from './catFoodCan.jsx'
@@ -28,8 +28,27 @@ export function Cafe() {
       <ImageBackground source={catBackground} contentFit="contain" style={{ width: '100%', height: 600 }}>
         <Cat ref={catRef} x={50} y={400} eatState={eatState} />
         <CatFoodCan ref={canRef} />
-        <Button title="Feed Cat" onPress={feedCat} />
+        {/* <Button title="Feed Cat" onPress={feedCat} /> */}
+        <TouchableOpacity style={styles.feedCatButton} onPress={feedCat}>
+          <Text>Feed Cat!</Text>
+        </TouchableOpacity>
       </ImageBackground>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  feedCatButton: {
+    height: 30,
+    width: 80,
+    position: 'absolute',
+    top: 550,
+    left: 150,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: 'rgb(228, 156, 3)',
+  }
+});
