bb7ac7bb51fe1e06879924d510790afb3ed7bc6f
Anxinal <ax1375240926@gmail.com>
2025-07-04 20:22:54 +0800

    [Feature] Outline the design the front end pages

---
+++ CyberCatMain/constants/RankingNames.js
@@ -0,0 +1,14 @@
+
+//Each element represents the name of its index's level
+export const names = [
+    'Novice',
+    'Emrald',
+    'Amethyst',
+    'Pearl',
+    'Diamond',
+    'Rainbow',
+];
+
+export const Icons = [
+    
+];
